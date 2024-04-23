7a5c32a9fb3c597d7a9a9323b02d645646ea72fd
Vidyashree <vidyashreemallesh28@gmail.com>
2024-04-23 22:07:26 +0530

    initial commit

--- median_of_2_sorted_arrays.py
+++ median_of_2_sorted_arrays.py
@@ -1,3 +1,6 @@
+'''Given two sorted arrays nums1 and nums2 of size m and n respectively, 
+return the median of the two sorted arrays.'''
+
 class Solution:
     def findMedianSortedArrays(self, nums1, nums2):
         # Merge the two sorted arrays into a single sorted array
