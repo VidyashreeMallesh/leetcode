872c9137eb3b28f927f70310047ee89225815313
Vidyashree <vidyashreemallesh28@gmail.com>
2024-06-23 15:12:23 +0530

    add solution for finding indices of two numbers in an array that sum to a target value

--- twosum.py
+++ twosum.py
@@ -3,13 +3,13 @@ return indices of the two numbers such that they add up to target.
 Each input has exactly one solution, that is same element should not be used twice.'''
 import array as arr  # Import the array module and alias it as 'arr'
 
-nums = arr.array('i', [5, 19, 3, 6])  # Define an array 'nums' with integer values
+numbers = arr.array('i', [5, 19, 3, 6])  # Define an array 'nums' with integer values
 target = 11  # Define the target sum to find
 found_match = False  # Flag to track if a match is found
-leng = len(nums)  # Get the length of the 'nums' array
+leng = len(numbers)  # Get the length of the 'nums' array
 for i in range(0, leng - 1):  # Loop through indices of 'nums' array from 0 to length - 1
     for j in range(i + 1, leng):  # Nested loop to compare each pair of numbers
-        if nums[i] + nums[j] == target:  # Check if the sum of the current pair equals the target
+        if numbers[i] + numbers[j] == target:  # Check if the sum of the current pair equals the target
             print("Match found by adding index", i, "and", j)  # Print the indices of the matching pair
             found_match = True  # Set flag to True if match is found
 
